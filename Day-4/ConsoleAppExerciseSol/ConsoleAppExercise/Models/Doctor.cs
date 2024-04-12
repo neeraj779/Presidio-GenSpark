3352d19bdf327c24bc182c430b1fbd433c2d692b
Neeraj <neeraj79108@gmail.com>
2024-04-12 15:37:12 +0530

    Add Day-4 Excercise Sol

---
+++ Day-4/ConsoleAppExerciseSol/ConsoleAppExercise/Models/Doctor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppExercise.Models
+{
+    internal class Doctor
+    {
+        /// <summary>
+        /// Gets or sets the id of the doctor.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the doctor.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the age of the doctor.
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Gets or sets the years of experience of the doctor.
+        /// </summary>
+        public int Experience { get; set; }
+
+        /// <summary>
+        /// Gets or sets the qualification of the doctor.
+        /// </summary>
+        public string Qualification { get; set; }
+
+        /// <summary>
+        /// Gets or sets the speciality of the doctor.
+        /// </summary>
+        public string Speciality { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Doctor"/> class with specified details.
+        /// </summary>
+        /// <param name="id">The id of the doctor.</param>
+        /// <param name="name">The name of the doctor.</param>
+        /// <param name="age">The age of the doctor.</param>
+        /// <param name="experience">The years of experience of the doctor.</param>
+        /// <param name="qualification">The qualification of the doctor.</param>
+        /// <param name="speciality">The speciality of the doctor.</param>
+        public Doctor(int id, string name, int age, int experience, string qualification, string speciality)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Experience = experience;
+            Qualification = qualification;
+            Speciality = speciality;
+        }
+
+        /// <summary>
+        /// Prints the details of the doctor.
+        /// </summary>
+        public void PrintDetails()
+        {
+            Console.WriteLine("#########################################");
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Experience: {Experience}");
+            Console.WriteLine($"Qualification: {Qualification}");
+            Console.WriteLine($"Speciality: {Speciality}");
+            Console.WriteLine("#########################################");
+        }
+    }
+}
---
+++ Day-4/ConsoleAppExerciseSol/ConsoleAppExercise/Program.cs
@@ -0,0 +1,85 @@
+﻿using ConsoleAppExercise.Models;
+
+namespace ConsoleAppExercise
+{
+    internal class Program
+    {
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="Doctor"/> class by taking details from the console.
+        /// </summary>
+        /// <param name="id">The id of the doctor.</param>
+        /// <returns>The newly created doctor instance.</returns>
+        public static Doctor CreateDoctorByTakingDetailsFromConsole(int id)
+        {
+            Console.WriteLine("Please enter the doctor's name:");
+            string name;
+            do
+            {
+                name = Console.ReadLine();
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Name cannot be empty. Please enter the doctor's name:");
+                }
+            } while (string.IsNullOrEmpty(name));
+
+            int age;
+            do
+            {
+                Console.WriteLine("Please enter the doctor's age:");
+            } while (!int.TryParse(Console.ReadLine(), out age) || age <= 0);
+
+            int experience;
+            do
+            {
+                Console.WriteLine("Please enter the doctor's years of experience:");
+            } while (!int.TryParse(Console.ReadLine(), out experience) || experience < 0);
+
+            string qualification;
+            do
+            {
+                Console.WriteLine("Please enter the doctor's qualification:");
+                qualification = Console.ReadLine();
+                if (string.IsNullOrEmpty(qualification))
+                {
+                    Console.WriteLine("Qualification cannot be empty. Please enter the doctor's qualification:");
+                }
+            } while (string.IsNullOrEmpty(qualification));
+
+            string speciality;
+            do
+            {
+                Console.WriteLine("Please enter the doctor's speciality:");
+                speciality = Console.ReadLine();
+                if (string.IsNullOrEmpty(speciality))
+                {
+                    Console.WriteLine("Speciality cannot be empty. Please enter the doctor's speciality:");
+                }
+            } while (string.IsNullOrEmpty(speciality));
+
+            return new Doctor(id, name, age, experience, qualification, speciality);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many doctors do you want to save?");
+            int numberOfDoctors;
+            while (!int.TryParse(Console.ReadLine(), out numberOfDoctors) || numberOfDoctors <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer value:");
+            }
+
+            Doctor[] doctors = new Doctor[numberOfDoctors];
+
+            for(int i = 0; i < doctors.Length; ++i)
+            {
+                doctors[i] = Program.CreateDoctorByTakingDetailsFromConsole(i);
+            }
+
+            Console.WriteLine("List of Doctors:");
+            for (int i = 0; i < doctors.Length; ++i)
+            {
+                doctors[i].PrintDetails();
+            }
+        }
+    }
+}
