643101ae066e18ea824b3bd99901a359ec6c4d40
Neeraj <neeraj79108@gmail.com>
2024-04-15 12:14:13 +0530

    Refactor project structure and rename files for better organization

---
+++ Day-5/RequestTrackerSol/RequestTrackerApplication/EmployeeProgram.cs
@@ -0,0 +1,21 @@
+﻿using RequestTrackerModelLibrary;
+
+namespace RequestTrackerApplication
+{
+    internal class EmployeeProgram
+    {
+        void CreateEmployee()
+        {
+            Employee employee = new Employee();
+            employee.Id = 101;
+            employee.BuildEmployeeFromConsole();
+            Console.WriteLine("----------------------------");
+            employee.PrintEmployeeDetails();
+        }
+        static void Main(string[] args)
+        {
+            EmployeeProgram employee_program = new EmployeeProgram();
+            employee_program.CreateEmployee();
+        }
+    }
+}
--- Day-5/RequestTrackerSol/RequestTrackerApplication/Program.cs
+++ Day-5/RequestTrackerSol/RequestTrackerApplication/Program.cs
@@ -1,4 +1,4 @@
-namespace RequestTrackerApp
+﻿namespace RequestTrackerApplication
 {
     internal class Program
     {
