e321b869c916547c6ee88dd39777c1390cecfc5e
Neeraj <neeraj79108@gmail.com>
2024-04-15 12:33:16 +0530

    Add EmployeeProgram class to add menu options and interaction

--- Day-5/RequestTrackerSol/RequestTrackerApplication/EmployeeProgram.cs
+++ Day-5/RequestTrackerSol/RequestTrackerApplication/EmployeeProgram.cs
@@ -4,18 +4,90 @@ namespace RequestTrackerApplication
 {
     internal class EmployeeProgram
     {
-        void CreateEmployee()
+        Employee[] employees;
+        public EmployeeProgram()
+        {
+            employees = new Employee[3];
+        }
+        void PrintMenu()
+        {
+            Console.WriteLine("1. Add Employee");
+            Console.WriteLine("2. Print Employees");
+            Console.WriteLine("0. Exit");
+        }
+        void EmployeeInteraction()
+        {
+            int choice = 0;
+            do
+            {
+                PrintMenu();
+                Console.WriteLine("Please select an option");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Bye.....");
+                        break;
+                    case 1:
+                        AddEmployee();
+                        break;
+                    case 2:
+                        PrintAllEmployees();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again");
+                        break;
+                }
+            } while (choice != 0);
+        }
+        void AddEmployee()
+        {
+            if (employees[employees.Length - 1] != null)
+            {
+                Console.WriteLine("Sorry we have reached the maximum number of employees");
+                return;
+            }
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i] == null)
+                {
+                    employees[i] = CreateEmployee(i);
+                }
+            }
+
+        }
+        void PrintAllEmployees()
+        {
+            if (employees[0] == null)
+            {
+                Console.WriteLine("No Employees available");
+                return;
+            }
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i] != null)
+                    PrintEmployee(employees[i]);
+            }
+        }
+        Employee CreateEmployee(int id)
         {
             Employee employee = new Employee();
-            employee.Id = 101;
+            employee.Id = 101 + id;
             employee.BuildEmployeeFromConsole();
-            Console.WriteLine("----------------------------");
-            employee.PrintEmployeeDetails();
+            return employee;
         }
+
+        void PrintEmployee(Employee employee)
+        {
+            Console.WriteLine("---------------------------");
+            employee.PrintEmployeeDetails();
+            Console.WriteLine("---------------------------");
+        }
+
         static void Main(string[] args)
         {
             EmployeeProgram employee_program = new EmployeeProgram();
-            employee_program.CreateEmployee();
+            employee_program.EmployeeInteraction();
         }
     }
 }
