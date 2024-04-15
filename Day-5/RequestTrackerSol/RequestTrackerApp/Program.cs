2d1470a37d62c019ae118a83d3d7f8063e02de54
Neeraj <neeraj79108@gmail.com>
2024-04-15 10:28:50 +0530

    Add Day-5 RequestTrackerApp code files

---
+++ Day-5/RequestTrackerSol/RequestTrackerApp/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace RequestTrackerApp
+{
+    internal class Program
+    {
+        void UnderstandingSequenceStatments()
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hi");
+            int num1, num2;
+            num1 = 100;
+            num2 = 20;
+            int num3 = num1 / num2;
+            Console.WriteLine($"The result of {num1} divided by {num2} is {num3}");
+        }
+
+        void UnderstandingSelectionWithIf()
+        {
+            string strName = "Ramu";
+            if (strName == "Ramu")
+            {
+                Console.WriteLine("Welcome Ramu. you are authorized");
+                Console.WriteLine("Bingo!!");
+            }
+            else if (strName == "Somu")
+                Console.WriteLine("You are Somu not Ramu. ONly Basic access");
+            else
+                Console.WriteLine("I don't know who you are. Get out...");
+        }
+
+        void UnderstandingSwithCase()
+        {
+            int choice = 2;
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thrusday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Opps you entered invaid number");
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            //program.UnderstandingSequenceStatments();
+            //program.UnderstandingSelectionWithIf();
+            program.UnderstandingSwithCase();
+        }
+    }
+}
