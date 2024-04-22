725ec3126b50200382431d2dcc6d3c911caab47f
Neeraj <neeraj79108@gmail.com>
2024-04-22 09:45:43 +0530

    Initial commit

---
+++ Day-9/SimpleBankingSol/SimpleBankingApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleBankingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
