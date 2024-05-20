6b0f76382006311997c448c1ca4e6c45f7147b55
Neeraj <neeraj79108@gmail.com>
2024-05-20 14:44:20 +0530

    Add Day-28 Work

---
+++ Day-28/EmployeeRequestTrackerAPI/EmployeeRequestTrackerAPI/Exceptions/NoEmployeesFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace EmployeeRequestTrackerAPI.Exceptions
+{
+    [Serializable]
+    internal class NoEmployeesFoundException : Exception
+    {
+        String _message;
+        public NoEmployeesFoundException()
+        {
+            _message = "No employees found";
+        }
+        public override string Message => _message;
+    }
+}
