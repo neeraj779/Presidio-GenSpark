93e2b52d1a2ecb7b7abbf5bd00fb06445232e594
Neeraj <neeraj79108@gmail.com>
2024-07-05 17:31:22 +0530

    Add July-05 work

---
+++ July-05/ClassWork/ShopAPI/Models/ShopContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShopAPI.Models
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
